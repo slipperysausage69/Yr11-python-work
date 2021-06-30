407e72746efad50e97fb5ab45f1838230ea627cc
Leon Haslar <LHaslar@stu.kerikerihigh.ac.nz>
2021-07-01 10:54:52 +1200

    fsdjkfbhjksd

--- quiz.py
+++ quiz.py
@@ -3,22 +3,25 @@
 score = 0
 
 print("\n\nWelcome to the epic car quiz")
-
 print("\nPlease asnswer each question in lower case Eg. a, b, c")
 
-q1 = input("\nQuestion 1 - What is faster in a quarter mile drag race\na. Porche 911 GT3 RS\nb. Honda Acura NSX\nc. Lamborghini Countach\n")
+# TODO make a function ask. Make it ask the question and only allow valid answers.
+# TODO give feedback to user if they give an invalid answer
+def ask(question, answers):
+    response = None
+    if response not in answers:
+        print("input actual answer")
+    while response not in answers:
+        response = input(question)
+    return response
 
+
+q1 = ask("\nQuestion 1 - What is faster in a quarter mile drag race\na. Porche 911 GT3 RS\nb. Honda Acura NSX\nc. Lamborghini Countach\n", ['a', 'b', 'c'])
 if q1 == "a":
     score = score + 5
-elif q1 == "b":
-    score = score
-elif q1 == "c":
-    score = score
-else:
-    score = score
+
 
 q2 = input("Question 2 - What did peugeot make before it made cars\na. Bicycles\nb. Pepper grinders\nc. Corsets\nd. Ammunition\ne. All of the above\n")
-
 if q2 == "a":
     score = score
 elif q2 == "b":
@@ -33,7 +36,6 @@ else:
     score = score
 
 q3 = input("Question 3 - What car holds the lap record for the Nurburgring\na. Ferrari Laferrari\nb. Bughatti Chiron\nc. Porche 919 Hybrid\nd. Mercedes W12 Formula 1 car\n")
-
 if q3 == "a":
     score = score
 if q3 == "b":
@@ -46,7 +48,6 @@ else:
     score = score
 
 q4 = input("Question 4 - When was the first car made\na. 1873\nb. 1886\nc. 1904\nd. 1914\n")
-
 if q4 == "a":
     score = score
 if q4 == "b":
@@ -58,8 +59,7 @@ if q4 == "d":
 else:
     score = score
 
-q5 = input("Question 5 - Which car has the fastest accelaration\na. BMW M5\nb.Audi RS5\nc. Mercedes c300 ")
-
+q5 = input("Question 5 - Which car has the fastest accelaration\na. BMW M5\nb. Audi RS5\nc. Mercedes c300\n ")
 if q5 == "a":
     score = score + 5
 if q5 == "b":
@@ -69,5 +69,44 @@ if q5 == "c":
 else:
     score = score
 
-print (score)
+q6 = input("Question 6 - What car won the last Bathurst race\na. Holden commodore VX\nb. Ford mustang MK6\nc. Holden commodore ZB\n")
+if q6 == "a":
+    score = score
+if q6 == "b":
+    score = score + 5
+if q6 == "c":
+    score = score
+else:
+    score = score
 
+q7 = input("Question 7 - What car company owns Porche\na. Maserati\nb. Audi\nc. Mercedes\nd. Volkswagen\n")
+if q7 == "a":
+    score = score
+if q7 == "b":
+    score = score
+if q7 == "c":
+    score = score
+if q7 == "d":
+    score = score + 5
+else:
+    score = score
+
+q8 = input("Question 8 - Which car will win in a quarte mile drag race\na. Porche 918 Spyder\nb. Mclaren P1\nc. Ferrari Laferrari")
+if q8 == "a":
+    score = score
+if q8 == "b":
+    score = score
+if q8 == "c":
+    score = score + 5
+else:
+    score = score
+
+q9 = input("Question 8 - ")
+if q9 == "a":
+    score = score
+if q9 == "b":
+    score = score
+if q9 == "c":
+    score = score + 5
+else:
+    score = score
